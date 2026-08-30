b696c8eddce6e674a55fa5d4b4e55bb58f279f1c
ajayganuga <ajayganuga@gmail.com>
2026-08-30 22:37:53 +0530

    Basics of Python

---
+++ 00-hello.py
@@ -0,0 +1,177 @@
+print("this is my first program in Python!")
+print("Hello, Ajay!")
+print (5+5)
+
+num = 5 + 2.0
+print (num)
+print (type(num))
+
+num_int =int (5.7)
+print (num_int)
+print (type(num_int))
+
+num_float = float("3.14")
+print (num_float)
+print (type(num_float))
+
+message = str("42")
+print (message)
+print (type(message))
+
+char_a = 'A'
+ascii_value_a = ord(char_a)
+print (ascii_value_a)
+
+char_b = 'B'
+ascii_value_b = ord(char_b)
+print ( ascii_value_b)  
+
+a = 65
+ascii_value = chr(a)
+print(ascii_value)
+
+
+# arithemetic operaters
+
+#additoon 
+
+a = 10
+b=20
+c=a+b
+print ("Addition:",c)
+
+# subtraction
+a=40
+b=30
+z=a-b
+print("Subtraction:",z)
+
+#multiplication
+
+a=40
+b=22
+y=b*a
+print("Multipliction:",y)
+
+#division 
+a=10
+f=3
+data=a/f
+print("Division:",data)
+
+#integer Division (floor Division)
+i=10
+k=10
+z=i//k
+print("integer Division (floor Division):",z)
+
+#Modulo (Remainder)
+a=50
+b=30
+c=a%b
+print("Modulo (Remainder):",c)
+
+#Exponentiation
+a=20
+b=7
+c=a**b
+print("Exponentiation:",c)
+
+#Assignment Operator (=)
+a=100
+b=500
+z=500
+print(a,b,z)
+
+#Add and Assign (+=)
+a=5
+b=10
+c=50
+a+=50
+print("Value of a after add and assign:",a)
+
+#Subtract and Assign (-=)
+a=5
+b=10
+c=50
+a-=3
+print("Value of a after subtract and assign:",a)
+
+# Multiply and Assign (*=)
+a=5
+b=10
+c=50
+a*=3
+print("Value of a after Multiply and assign:",a)
+
+# Divide and Assign (/=)
+a=5
+b=10
+c=50
+a/=3
+print("Value of a after  Divide and assign:",a)
+
+# Integer Divide and Assign (//=)
+a=5
+b=10
+c=50
+a//=3
+print("Value of a after Integer Divide and assign:",a)
+
+# modulo and Assign (%=)
+a=5
+b=10
+c=50
+a%=3
+print("Value of a after modulo and assign:",a)
+
+# Exponentiation and Assign (**=)
+a=5
+b=10
+c=50
+a**=3
+print("Value of a after modulo and assign:",a)
+
+##### Comparision Operators 
+#_____________________________
+# 1. (== Equal to )
+
+a = 10 
+b = 20 
+c = a==b
+print (c)
+
+
+# 2. (!= Equal to )
+
+a = 10 
+b = 20 
+c = a!=b
+print (c)
+
+# 3. (> Greater Than )
+
+a = 10 
+b = 20 
+c = a>b
+print (c)
+
+# 4. (< Less Than )
+
+a = 10 
+b = 20 
+c = a<b
+print (c)
+
+# 5. (>= Greater Than or Equal to ) 
+a = 10 
+b = 20 
+c = a>=b
+print ("is a is Greater than or Equal b:", c)
+
+# 6. (<= Less Than or Equal to ) 
+a = 10 
+b = 20 
+c = a<=b
+print ("is a is Less than or Equal b:", c)
+
